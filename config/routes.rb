26994de1ee5a5b3c283299770e13486e0dc0f374
Rodman Aguirre Méndez <rodfagui@gmail.com>
2017-04-19 17:58:56 -0500

    Agrega enlace para mostrar los detalles del producto

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/products', to: 'products#index'
   get '/products/new', to: 'products#new', as: 'new_product'
   post '/products', to: 'products#create'
-  get '/products/:id', to: 'products#show'
-  get '/products/:id/edit', to: 'products#edit', as: 'product'
+  get '/products/:id', to: 'products#show', as: 'product'
+  get '/products/:id/edit', to: 'products#edit'
   patch '/products/:id/edit', to: 'products#update'
 end
